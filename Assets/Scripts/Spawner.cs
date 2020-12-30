9367e2c411e470afc5b3b46ef8a16958a967009a
Kwicot <kwicot@gmail.com>
2020-12-30 21:49:31 +0200

    Доделал спавнер и таймер волн

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,32 +5,70 @@ using UnityEngine.UI;
 
 public class Spawner : MonoBehaviour
 {
+    public GameObject EnemyPrefab;
+    public Image GreenProgressBar;
+    public Image RedProgressBar;
+
+
     public int EnemyPerWave { get; set; }
     public int BossWave { get; set; }
     public int CurrentWave { get; }
-    public float Timer { get; set; }
     public float PauseBetweenWave { get; set; }
-    public List<Enemy> L_Enemy { get; set; }
+    public List<GameObject> L_Enemy { get; set; }
 
 
 
     List<Vector3> L_SpawnPoints = new List<Vector3>();
 
+
+    float Timer;
+    bool CanTime;
+
     void Start()
     {
-        L_Enemy = new List<Enemy>();
+        //EnemyPerWave = 100;
+        //PauseBetweenWave = 10;
+
+
+        L_Enemy = new List<GameObject>();
         GameObject[] points = GameObject.FindGameObjectsWithTag("SpawnPoint");
         for(int i = 0; i <points.Length -1; i++)
         {
             L_SpawnPoints.Add(points[i].transform.position);
-            Destroy(points[i]);
         }
+        CanTime = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (CanTime)
+        {
+            RedProgressBar.fillAmount = Timer / PauseBetweenWave;
+            Timer += Time.deltaTime;
+            if (Timer > PauseBetweenWave) NewWave();
+        }
+    }
 
+    IEnumerator NewWave()
+    {
+        RedProgressBar.fillAmount = 0;
+        Timer = 0;
+        CanTime = false;
+        
+        for(int i = 0, x = 0; i< EnemyPerWave ;x++, i++)
+        {
+            if (x == L_SpawnPoints.Count) x = 0;
+            GameObject obj = Instantiate(EnemyPrefab, L_SpawnPoints[x], Quaternion.identity);
+            L_Enemy.Add(obj);
+            GreenProgressBar.fillAmount = i / EnemyPerWave;
+            yield return new WaitForSeconds(0.05f);
+        }
+
+    }
+    public void SkipWave()
+    {
+        StartCoroutine(NewWave());
     }
 
 

9412ff5d6e0401f9eb8f2cc18099c76467accf54
muaddib <muddib.fremen@gmail.com>
2021-01-08 22:15:04 +0200

    добавил окно перехода в город

---
+++ Assets/OnOnableElement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class OnOnableElement : MonoBehaviour
+{
+    public GameObject[] show;
+    public GameObject[] hide;
+
+    private void OnEnable()
+    {
+        if(show != null)
+            foreach (var e in show)
+            {
+                if (e == null) continue;
+                e.SetActive(true);
+            }
+        
+        if(hide != null)
+            foreach (var e in hide)
+            {
+                if (e == null) continue;
+                e.SetActive(false);
+            }
+    }
+}

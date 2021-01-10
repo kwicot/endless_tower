73ab2c7042b5eda491c9c0d51c1274923a093959
Kwicot <kwicot@gmail.com>
2021-01-10 10:37:05 +0200

    доделал панель локальных улучшений

--- Assets/Scripts/Model/Tower.cs
+++ Assets/Scripts/Model/Tower.cs
@@ -2,11 +2,12 @@
 [System.Serializable]
 public class Tower
 {
-    public float AttackRange;
-    public float AttackSpeed;
     public float Damage;
+    public float Impetus;
     public float CriticalDamageChance;
     public float CriticalDamageMultiplier;
+    public float AttackSpeed;
+    public float AttackRange;
     public float MultiFireChance;
     public int MultiFireTargets;
     public float RapidFireChance;
@@ -16,6 +17,11 @@ public class Tower
     public int BounceCountMax;
     public float BlackHoleChance;
     public float BlackHoleTime;
+    public float FireBallChance;
+    public float FireBallDamage;
+    public float IceBallChance;
+    public float IceBallDamage;
+    public float IceBallFreezeTime;
 
 
     public float HP;
@@ -23,13 +29,24 @@ public class Tower
     public float Regeneration;
     public float Defense;
     public float BlockChance;
-    public float BlockDamage;
     public float SlowAuraRange;
     public float SlowAuraPerfomance;
     public float DamageAuraRange;
     public float DamageAuraPerfomance;
     public float DamageIncreaseAuraRange;
     public float DamageIncreaseAuraPerfomance ;
+    public float BOOMChance;
+
+
+
+    public float BonusWhiteCubePerKill;
+    public float BonusWhiteCubePerWave;
+    public float BonusOrangeCubePerKill;
+    public float BonusOrangeCubePerWave;
+    public float ChanceCrystalPerKill;
+    public float ChanceCrystalPerWave;
+    public float AutoCollectorSpeed;
+    public float AutoCollectorRange;
 
 
 
